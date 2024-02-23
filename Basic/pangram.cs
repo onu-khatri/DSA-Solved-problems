12be8ae8b44edd467e5bfa1ed629b304a28892e7
onu-khatri <85816412+onu-khatri@users.noreply.github.com>
2024-02-23 16:58:06 +0530

    Create pangram.cs

---
+++ Basic/pangram.cs
@@ -0,0 +1,51 @@
+/*
+A pangram is a string that contains every letter of the alphabet. Given a sentence determine whether it is a pangram in the English alphabet. 
+Ignore case. Return either pangram or not pangram as appropriate.
+*/
+using System;
+
+public class Result
+{
+    public static string pangrams(string s)
+    {
+        var characters = s.ToLower().ToCharArray();
+        var reverseIndex = characters.Length - 1;
+        var alphaArray = new int[26];
+        var totalKeyFound = 0;
+        
+		var processCharAscii = (int index, char character, int charAscii) => {
+			if(character != ' ' && alphaArray[index] == 0){
+               totalKeyFound++; 
+			   alphaArray[index] = charAscii;
+            }
+		};
+		
+        for(var i=0; i< characters.Length; i++) {
+            var leftCharAsci = (int)characters[i];
+			processCharAscii(leftCharAsci-97, characters[i], leftCharAsci);
+            
+            if(i >= reverseIndex) {
+                break;
+            }
+            
+            var rightCharAsci = (int)characters[reverseIndex];
+			processCharAscii(rightCharAsci-97, characters[reverseIndex], rightCharAsci);
+			
+			reverseIndex--;
+        }
+        
+        return totalKeyFound == 26 ? "pangram": "not pangram";
+    }
+}
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        string result = Result.pangrams("We promptly judged antique ivory buckles for the prize");
+        Console.WriteLine(result);
+		
+		string result1 = Result.pangrams("the quick brown fox jumnps over a lazy dog");
+        Console.WriteLine(result1);
+    }
+}
